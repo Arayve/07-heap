302f69061f42b9d7774880b241d0eecf9bd02c7b
Arayve <alejandro150998@outlook.com>
2018-05-29 16:03:04 -0300

    Agregando upheap

--- heap.c
+++ heap.c
@@ -13,9 +13,30 @@ struct heap{
 	size_t cantidad;
 	size_t capacidad;
 };
+void _swap(void* arreglo1,void* arreglo2){
+	void* aux = arreglo1;
+	arreglo1 = arreglo2;
+	arreglo2 = aux;
+}
 void _upheap(void *arreglo[],size_t pos ,cmp_func_t cmp){
 	if(pos == 0) return;
 	size_t padre = (pos-1)/2;
+	int comparacion cmp(arreglo[pos],arreglo[padre]);
+	if(comparacion < 0)return;
+	_swap(arreglo[pos],arreglo[padre]);
+	_upheap(arreglo,padre,cmp);
+}
+void _downheap(void *arreglo[],size_t n ,size_t pos ,cmp_func_t cmp){// n creo es donde termina el arreglo
+	if(pos >= n) return;
+	size_t = pos_hijo_izq =  2*pos+1;
+	size_t = pos_hijo_der =  2*pos+1;
+	size_t = pos_max = pos;
+	if(pos_hijo_izq < n && cmp(arreglo[pos_hijo_izq],arreglo[pos_max])){
+		pos_max = pos_hijo_izq;
+	}
+	if(pos_hijo_der < n && cmp(arreglo[pos_hijo_der],arreglo[pos_max])){
+		pos_max = pos_hijo_der;
+	}
 }
 heap_t* heap_crear(cmp_func_t cmp){
 	heap_t* heap = malloc(sizeof(heap_t));
