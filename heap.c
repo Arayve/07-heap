3d2bbe643162e578b7a4c37ed223dbd42cf3bc34
KethTatsu <ben.inneo@gmail.com>
2018-05-31 18:58:58 -0300

    Arreglado swap en heap.c (y otros cambios mínimos), agregadas pruebas en pruebas_alumno.c (faltan pruebas del heap_sort)

--- heap.c
+++ heap.c
@@ -12,38 +12,40 @@
 //padre = (pos -1 )/2
 // ult padre = (largo/2)-1
 struct heap{
-	void** tabla;
+	void** datos;
 	cmp_func_t cmp;
 	size_t cantidad;
 	size_t capacidad;
 };
-void _swap(void* arreglo1,void* arreglo2){
-	void* aux = arreglo1;
-	arreglo1 = arreglo2;
-	arreglo2 = aux;
+
+void _swap(void* datos[], size_t pos_1, size_t pos_2){
+	void* aux = datos[pos_1];
+	datos[pos_1] = datos[pos_2];
+	datos[pos_2] = aux;
 }
-void _upheap(void *arreglo[],size_t pos ,cmp_func_t cmp){
+void _upheap(void *datos[], size_t pos, cmp_func_t cmp){
 	if(pos == 0) return;
 	size_t padre = (pos-1)/2;
-	int comparacion = cmp(arreglo[pos],arreglo[padre]);
-	if(comparacion < 0)return;
-	_swap(arreglo[pos],arreglo[padre]);
-	_upheap(arreglo,padre,cmp);
+
+	if(cmp(datos[padre], datos[pos]) > 0) return;
+
+	_swap(datos, padre, pos);
+	_upheap(datos,padre,cmp);
 }
-void _downheap(void *arreglo[],size_t n ,size_t pos ,cmp_func_t cmp){// n creo es donde termina el arreglo
+void _downheap(void *datos[],size_t n ,size_t pos ,cmp_func_t cmp){// n creo es donde termina el arreglo
 	if(pos >= n) return;
-	size_t  pos_hijo_izq =  2*pos+1;
-	size_t  pos_hijo_der =  2*pos+1;
+	size_t  pos_hijo_izq = 2*pos+1;
+	size_t  pos_hijo_der = 2*pos+2;
 	size_t  pos_max = pos;
-	if(pos_hijo_izq < n && cmp(arreglo[pos_hijo_izq],arreglo[pos_max])>0){
+	if(pos_hijo_izq < n && cmp(datos[pos_hijo_izq],datos[pos_max])>0){
 		pos_max = pos_hijo_izq;
 	}
-	if(pos_hijo_der < n && cmp(arreglo[pos_hijo_der],arreglo[pos_max])>0){
+	if(pos_hijo_der < n && cmp(datos[pos_hijo_der],datos[pos_max])>0){
 		pos_max = pos_hijo_der;
 	}
 	if(pos != pos_max){
-		_swap(arreglo[pos],arreglo[pos_max]);
-		_downheap(arreglo,n,pos_max,cmp);
+		_swap(datos, pos, pos_max);
+		_downheap(datos,n,pos_max,cmp);
 	}
 }
 heap_t* heap_crear(cmp_func_t cmp){
@@ -52,8 +54,8 @@ heap_t* heap_crear(cmp_func_t cmp){
 	heap->cantidad = CANTIDAD_INICIAL;
 	heap->capacidad = CAPACIDAD_INICIAL;
 	heap->cmp = cmp;
-	heap->tabla = malloc((heap->capacidad)*sizeof(void*));
-	if(!heap->tabla){
+	heap->datos = malloc((heap->capacidad)*sizeof(void*));
+	if(!heap->datos){
         free(heap);
         return NULL;
     }
@@ -64,27 +66,31 @@ heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp){
 	if(!heap) return NULL;
 	size_t pos_act = 0;
 	while(pos_act < n){
-		if(!heap_encolar(heap,arreglo[pos_act]))return NULL;
+		if(!heap_encolar(heap,arreglo[pos_act])){
+			heap_destruir(heap, NULL);
+			return NULL;
+		}
+		pos_act++;
 	}
 	return heap;
 }
-bool _redimenzinar_heap(heap_t *heap, size_t tam_nuevo){
-	void** tabla_nueva = realloc (heap->tabla, tam_nuevo*sizeof(void*));
+bool _redimensionar_heap(heap_t *heap, size_t tam_nuevo){
+	void** datos_nuevos = realloc(heap->datos, tam_nuevo*sizeof(void*));
 	if(!tam_nuevo) return false;
-	heap->tabla = tabla_nueva;
+	heap->datos = datos_nuevos;
 	heap->capacidad = tam_nuevo;
-	return true;
 
+	return true;
 }
 bool heap_encolar(heap_t *heap, void *elem){
 	if(heap->cantidad == heap->capacidad){
-		size_t tam_nuevo = heap->capacidad * REDIMENSIONADOR;
-		if(!_redimenzinar_heap(heap,tam_nuevo)){
+		size_t tam_nuevo = heap->capacidad*REDIMENSIONADOR;
+		if(!_redimensionar_heap(heap,tam_nuevo)){
 			return false;
 		}
 	}
-	heap->tabla[heap->cantidad]= elem;
-	_upheap(heap->tabla,heap->cantidad,heap->cmp);
+	heap->datos[heap->cantidad] = elem;
+	_upheap(heap->datos,heap->cantidad,heap->cmp);
 	++heap->cantidad;
 	return true;
 }
@@ -92,31 +98,26 @@ size_t heap_cantidad(const heap_t *heap){
 	return(heap->cantidad);
 }
 bool heap_esta_vacio(const heap_t *heap){
-	if(heap->cantidad == 0)return true;
-	return false;
+	return (heap_cantidad(heap) == 0);
 }
 void *heap_ver_max(const heap_t *heap){
-	if(!heap)return NULL;
-	if(heap->cantidad == 0)return NULL;
-	return(heap->tabla[POSICION_DEL_MAX]);
+	if(heap_esta_vacio(heap)) return NULL;
+	return(heap->datos[POSICION_DEL_MAX]);
 }
 void *heap_desencolar(heap_t *heap){
-	if(!heap) return NULL;
-	if(heap->cantidad == 0 )return NULL;
-	if(heap->cantidad < heap->capacidad/TAMANIO_RELATIVO && CAPACIDAD_INICIAL < heap->cantidad){
+	if(heap_esta_vacio(heap)) return NULL;
+	if(heap->cantidad < heap->capacidad/TAMANIO_RELATIVO && CAPACIDAD_INICIAL < heap->capacidad){
 		size_t tam_nuevo = heap->capacidad/REDIMENSIONADOR;
-		if(!_redimenzinar_heap(heap,tam_nuevo)){
+		if(!_redimensionar_heap(heap,tam_nuevo)){
 			return false;
 		}
 	}
-	void* elem = heap->tabla[POSICION_DEL_MAX];
-	
-	size_t pos_ultimo_elemento = heap->cantidad-1;
-	_swap(heap->tabla[POSICION_DEL_MAX],heap->tabla[pos_ultimo_elemento]);
-	heap->tabla[pos_ultimo_elemento] = NULL;
-	-- heap->cantidad;
-	-- pos_ultimo_elemento; 
-	_downheap(heap->tabla,pos_ultimo_elemento,POSICION_DEL_MAX,heap->cmp);
+	void* elem = heap->datos[POSICION_DEL_MAX];
+
+	_swap(heap->datos, POSICION_DEL_MAX, heap->cantidad-1);
+	heap->datos[heap->cantidad-1] = NULL;
+	--heap->cantidad;
+	_downheap(heap->datos,heap->cantidad,POSICION_DEL_MAX,heap->cmp);
 	
 	return elem;
 }
@@ -127,7 +128,7 @@ void heap_destruir(heap_t *heap, void destruir_elemento(void *e)){
 			destruir_elemento(elem);
 		}
 	}
-	free(heap->tabla);
+	free(heap->datos);
     free(heap);
 }
 void heap_sort(void *elementos[], size_t cant, cmp_func_t cmp){
--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -2,37 +2,223 @@
 #include "testing.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void pruebas_heap_0_elementos() {
+#define TAM 5000
+#define TAM_ARREGLO 10
 
- 	printf("~~~ PRUEBAS 0 elemento ~~~\n" );
+int cmp_str(const void* a, const void* b){
+
+	return strcmp(a, b);
+}
+
+int cmp_int(const void* A, const void* B){
+
+	const int* a = A, *b = B;
+
+	if(*a < *b) return -1;
+	if(*a > *b) return 1;
+
+	return 0;
+}
+
+void prueba_heap_vacio() {
+
+ 	printf("~~~ PRUEBA HEAP VACIO ~~~\n" );
  	heap_t* heap = heap_crear(NULL);
 
- 	print_test("Heap disntinto a NULL", heap != NULL);
-    print_test("Heap debe estar vacia", heap_esta_vacio(heap));
-    print_test("El puntero eliminado debe ser NULL",heap_desencolar(heap) == NULL);
+ 	print_test("Se creó heap", heap != NULL);
+    print_test("Heap esta vacio", heap_esta_vacio(heap));
+    print_test("Heap cantidad es 0", heap_cantidad(heap) == 0);
+    print_test("heap_desencolar devuelve NULL",heap_desencolar(heap) == NULL);
+    print_test("heap_ver_max devuelve NULL", heap_ver_max(heap) == NULL);
     heap_destruir(heap,NULL);
-    print_test("La Heap fue destruido",true);
-
+    print_test("heap fue destruido",true);
 }
+
+void prueba_heap_1_elemento(){
+
+	printf("\n~~~ PRUEBA HEAP 1 ELEMENTO ~~~\n");
+
+	heap_t* heap = heap_crear(cmp_str);
+
+	char* palabra = "Hola mundo";
+
+	print_test("Se encoló palabra", heap_encolar(heap, palabra));
+	print_test("Ver max es palabra", heap_ver_max(heap) == palabra);
+	print_test("Heap cantidad es 1", heap_cantidad(heap) == 1);
+	print_test("Se desencoló palabra", heap_desencolar(heap) == palabra);
+	print_test("Heap está vacio", heap_esta_vacio(heap));
+
+	heap_destruir(heap, NULL);
+
+	printf("\n~~~ PRUEBA HEAP 1 ELEMENTO (DYNAMIC)\n");
+
+	heap = heap_crear(cmp_str);
+
+	char* palabra2 = malloc(10*sizeof(char));
+
+	print_test("Se encoló palabra2", heap_encolar(heap, palabra2));
+	print_test("Ver max es palabra", heap_ver_max(heap) == palabra2);
+	print_test("Heap cantidad es 1", heap_cantidad(heap) == 1);
+
+	heap_destruir(heap, free);
+}
+
 void pruebas_heap_5_elementos(){
-	printf("~~~ PRUEBAS 5 elemento ~~~\n" );
- 	heap_t* heap = heap_crear(strcmp);
- 	char* elem1 = "A";
-	char* elem2 = "B";
-	char* elem3 = "C";
-	char* elem4 = "D";
-	char* elem5 = "E";
-	print_test("El Encolo elemento",heap_encolar(heap,elem1));
-	print_test("El Encolo elemento",heap_encolar(heap,elem2));
-	print_test("El Encolo elemento",heap_encolar(heap,elem3));
-	print_test("El Encolo elemento",heap_encolar(heap,elem4));
-	print_test("El Encolo elemento",heap_encolar(heap,elem5));
-	print_test("El Desencolar , debe ser 5", heap_desencolar(heap) == elem5);
+	printf("\n~~~ PRUEBAS 5 elemento ~~~\n" );
+ 	heap_t* heap = heap_crear(cmp_str);
+ 	char* elem1 = "Arg";
+	char* elem2 = "Bra";
+	char* elem3 = "Ale";
+	char* elem4 = "Nig";
+	char* elem5 = "Jap";
+
+	print_test("Se encoló elem1",heap_encolar(heap,elem1));
+	print_test("heap_cantidad es 1", heap_cantidad(heap) == 1);
+	print_test("Se encoló elem2",heap_encolar(heap,elem2));
+	print_test("heap_cantidad es 2", heap_cantidad(heap) == 2);
+	print_test("Se encoló elem3",heap_encolar(heap,elem3));
+	print_test("heap_cantidad es 3", heap_cantidad(heap) == 3);
+	print_test("Se encoló elem4",heap_encolar(heap,elem4));
+	print_test("heap_cantidad es 4", heap_cantidad(heap) == 4);
+	print_test("Se encoló elem5",heap_encolar(heap,elem5));
+	print_test("heap_cantidad es 5", heap_cantidad(heap) == 5);
+
+	print_test("elem4 es maximo", heap_ver_max(heap) == elem4);
+	print_test("Se desencoló elem4", heap_desencolar(heap) == elem4);
+	print_test("heap_cantidad es 4", heap_cantidad(heap) == 4);
+	print_test("elem5 es maximo", heap_ver_max(heap) == elem5);
+	print_test("Se desencoló elem5", heap_desencolar(heap) == elem5);
+	print_test("heap_cantidad es 3", heap_cantidad(heap) == 3);
+	print_test("elem2 es maximo", heap_ver_max(heap) == elem2);
+	print_test("Se desencoló elem2", heap_desencolar(heap) == elem2);
+	print_test("heap_cantidad es 2", heap_cantidad(heap) == 2);
+	print_test("elem1 es maximo", heap_ver_max(heap) == elem1);
+	print_test("Se desencoló elem1", heap_desencolar(heap) == elem1);
+	print_test("heap_cantidad es 1", heap_cantidad(heap) == 1);
+	print_test("elem3 es maximo", heap_ver_max(heap) == elem3);
+	print_test("Se desencoló elem3", heap_desencolar(heap) == elem3);
+	print_test("Heap está vacio", heap_esta_vacio(heap));
+
 	heap_destruir(heap,NULL);
+}
+/*
+void prueba_heap_sort(){
+
+	printf("\n");
+
+	void** numeros;
+
+	int num = 1, num2 = 4, num3 = 2, num4 = 100, num5 = 92, num6 = 22, num7 = 7, num8 = 80, num9 = 0, num10 = 42;
+
+	numeros[0] = &num;
+	numeros[1] = &num2;
+	numeros[2] = &num3;
+	numeros[3] = &num4;
+	numeros[4] = &num5;
+	numeros[5] = &num6;
+	numeros[6] = &num7;
+	numeros[7] = &num8;
+	numeros[8] = &num9;
+	numeros[9] = &num10;
+
+	heap_sort(numeros, 10, cmp_int);
+
+	for(size_t i = 0; i < 10; i++){
+		printf("| %d |", numeros[i]);
+	}
+
+	printf("\n");
+}
+*/
+void prueba_heap_volumen(){
+
+	printf("\n~~~ PRUEBA HEAP VOLUMEN~~~\n");
+
+	heap_t* heap = heap_crear(cmp_int);
+
+	int numeros[TAM], tope = TAM;
+
+	for(int i = 0; i < tope; i++) numeros[i] = i;
+
+	for(size_t i = 0; i < tope; i++){
+		if(!heap_encolar(heap, &numeros[i])){
+			print_test("NO se pudieron encolar los elementos", false);
+			break;
+		}
+		if(!(heap_ver_max(heap) == &numeros[i])){
+			print_test("NO es el maximo", false);
+			break;
+		}
+		if(heap_cantidad(heap) != i+1){
+			print_test("heap_cantidad en volumen", false);
+			break;
+		}
+	}
+
+	print_test("Se pudieron encolar los elementos", true);
+	print_test("heap_cantidad en volumen (5000)", heap_cantidad(heap) == tope);
+
+	for(size_t i = tope; i > 0; i--){
+		if(!(heap_ver_max(heap) == &numeros[i-1])){
+			print_test("NO es el maximo", false);
+			break;
+		}
+		if(!(heap_desencolar(heap) == &numeros[i-1])){
+			print_test("NO se pudieron desencolar los elementos", false);
+			break;
+		}
+		if(!(heap_cantidad(heap) == i-1)){
+			print_test("heap_cantidad en volumen", false);
+			break;
+		}
+	}
+
+	print_test("Se desencolaron todos los elementos", heap_cantidad(heap) == 0);
+	print_test("Heap está vacio", heap_esta_vacio(heap));
+
+	heap_destruir(heap, NULL);
+}
+
+void prueba_heap_crear_arreglo(){
+
+	printf("\n~~~ PRUEBA HEAP CREAR ARREGLO ~~~\n");
+
+	void* arreglo[TAM_ARREGLO];
+
+	arreglo[0] = "Argentina";
+	arreglo[1] = "Uruguay";
+	arreglo[2] = "Paraguay";
+	arreglo[3] = "Peru";
+	arreglo[4] = "Brasil";
+	arreglo[5] = "Mexico";
+	arreglo[6] = "Cuba";
+	arreglo[7] = "Colombia";
+	arreglo[8] = "Venezuela";
+	arreglo[9] = "Nicaragua";
+
+	heap_t* heap = heap_crear_arr(arreglo, TAM_ARREGLO, cmp_str);
+
+	print_test("Se creó heap", heap != NULL);
+
+	print_test("Heap ver maximo (Venezuela)", heap_ver_max(heap) == arreglo[8]);
+	print_test("Se desencoló (Venezuela)", heap_desencolar(heap) == arreglo[8]);
+	print_test("Heap ver maximo (Uruguay)", heap_ver_max(heap) == arreglo[1]);
+
+	char* extra = "Yugoslavia";
+
+	print_test("Se encoló extra", heap_encolar(heap, extra));
+	print_test("Heap ver maximo (Yugoslavia)", heap_ver_max(heap) == extra);
+
+	heap_destruir(heap, NULL);
 }
 
 void pruebas_heap_alumno(){
-	pruebas_heap_0_elementos();
+	prueba_heap_vacio();
+	prueba_heap_1_elemento();
 	pruebas_heap_5_elementos();
+//	prueba_heap_sort();
+	prueba_heap_volumen();
+	prueba_heap_crear_arreglo();
 }
